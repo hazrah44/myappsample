845562705c56285f38c6607e037b54e5ded87fcf
hazrah othman <me@hazrahothman.com>
2021-12-08 04:53:03 -0500

    update

--- dir1/app1.js
+++ dir1/app1.js
@@ -1,15 +1,16 @@
 //console.log(123);
 //Assignment 5 - Part 2
-async function getDegrees(url) {
+async function getDegrees() {
     // fetch the url
+    
     await fetch(url)
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
       .then((data) =>
         document.write(
           `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date} and Master in Software Development in ${data.data[0].degrees.bachelors.data.futuregraduation}`
-        )
+      )
       );
-  }
+        }
   
   getDegrees("./dir1/degrees.json");
