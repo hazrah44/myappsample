75fc145641106df4f81fc083296980cef079dad9
hazrah othman <me@hazrahothman.com>
2021-12-08 09:38:26 -0500

    update

---
+++ html/index.js
@@ -0,0 +1,15 @@
+//console.log(123);
+//Assignment 5 - Part 2
+async function getDegrees() {
+    // fetch the url
+    await fetch(url)
+      //get your data here, and check for the response status. If it's not 200, throw an error
+      .then((response) => response.json())
+      .then((data) =>
+        document.write(
+          `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date} and Master in Software Development in ${data.data[0].degrees.bachelors.data.futuregraduation}`
+      )
+      );
+        }
+  
+  getDegrees("./src/degrees.json");
